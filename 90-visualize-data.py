4366cd86e8e15fd3b845cefc1278be6befb9e855
Florian Golemo <fgolemo@gmail.com>
2017-10-10 17:06:50 -0400

    dunno where all these changes come from

--- 90-visualize-data.py
+++ 90-visualize-data.py
@@ -3,7 +3,9 @@ from PIL import Image
 import numpy as np
 import cv2
 
-FILE_NAME= "data/data_dump.npz"
+# FILE_NAME= "data/data_dump_5.npz" # avg 80
+# FILE_NAME= "data/data_dump_95.npz" # avg 17
+FILE_NAME= "data/data_dump_137.npz" # avg 60
 OUT_PATH = "tests/out/"
 
 data = np.load(FILE_NAME)
@@ -15,13 +17,22 @@ print (kinect.shape)
 print (robo.shape)
 print (kinect[0].shape)
 
+kl = []
+for i in range(len(kinect)):
+    kl.append(len(kinect[i]))
+
+print ("sum:",sum(kl))
+print ("avg:",sum(kl)/len(kl))
+
 # quit()
+
+
 # for i in range(3):
 #     for j in range(200):
 #         print (np.around(robo[j,i,:,0]))
 
-kinect = kinect[120]
-robo = robo[120]
+kinect = kinect[15]
+robo = robo[15]
 
 # iterate over kinect frames and render them to disk
 for i in range(len(kinect)):
@@ -35,7 +46,7 @@ for i in range(len(kinect)):
 
     # img.show()
     # quit()
-    img.save(OUT_PATH+'img/{:03d}.png'.format(i))
+    img.save(OUT_PATH+'img-137/{:03d}.png'.format(i))
 
 
 
