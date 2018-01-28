550da0b9c6be8dc96a11197139efee1a4f5f50b4
Florian Golemo <fgolemo@gmail.com>
2018-01-28 18:52:04 -0500

    added better image script and dataset test

--- 96-create-real-dataset.py
+++ 96-create-real-dataset.py
@@ -11,6 +11,9 @@ from config.constants import WRITE_EVERY_N_EPISODES
 from movements.dataset import Dataset
 from vrepper.core import vrepper
 import matplotlib.pyplot as plt
+import h5py
+
+f = h5py.File("data/test-dataset.hdf5", "w")
 
 FILE_IDX = 145
 MAX_FRAMES = 273  # max episode length is 274, so -1 (because we always look one step ahead) is 273
@@ -47,7 +50,7 @@ def set_motors(positions, speeds=None):
             m.set_velocity(speeds[i])
 
 
-def get_motors(with_velocities=False):
+def get_motors():
     out_pos = np.zeros(6, dtype=np.float32)
     out_vel = np.zeros(6, dtype=np.float32)
     for i, m in enumerate(motors):
@@ -55,9 +58,8 @@ def get_motors(with_velocities=False):
         if i == 0:
             angle *= -1
         out_pos[i] = angle
+        out_vel[i] = m.get_joint_velocity()[0]
 
-        if with_velocities:
-            out_vel[i] = m.get_velocity()
     return out_pos, out_vel
 
 
@@ -75,15 +77,21 @@ def reset_sim(initial_position=None):
 
 
 def real_img_reshaping(img_in):
-    return np.rot90(
-        np.swapaxes(
-            img_in[:, :, ::-1].T,  # BGR -> RGB & transpose
-            0,  # move color axis to the right
-            2
-        ),
+    img_depth = img_in[:, :, 3]
+    img_color = img_in[:, :, :3]
+    img_combined = np.zeros(img_in.shape, dtype=np.uint8)
+    img_combined[:, :, :3] = np.rot90(
+        img_color[:, :, ::-1],  # BGR -> RGB
         2,  # rotate 180...
         (0, 1)  # ...on the x-y plane
     )
+    img_combined[:, :, 3] = np.rot90(
+        img_depth,
+        2,  # rotate 180...
+        (0, 1)  # ...on the x-y plane
+    )
+
+    return img_combined
 
 
 ds = Dataset()
@@ -91,22 +99,22 @@ ds.load(DATASET_PATH_CLEAN)
 data = np.load(FILE_NAME)
 real_imgs, real_positions, real_speeds = data["real_images"], data["real_positions"], data["real_speeds"]
 
-dataset_current_pos_real = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)
-dataset_current_vel_real = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)
-
-dataset_next_pos_sim = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)  # this is LSTM input
-dataset_next_vel_sim = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)  # this is LSTM input
-
-dataset_next_pos_real = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)  # this is LSTM output
-dataset_next_vel_real = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)  # this is LSTM output
-
-dataset_current_action = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)  # this is LSTM input/conditioning
-dataset_current_speed = np.zeros((EPISODES, MAX_FRAMES), dtype=np.float32)
-
-dataset_current_img_real = np.zeros((EPISODES, MAX_FRAMES, IMG_REAL_HEIGHT, IMG_REAL_WIDTH, 4), dtype=np.uint8)
-
-dataset_next_img_real = np.zeros((EPISODES, MAX_FRAMES, IMG_REAL_HEIGHT, IMG_REAL_WIDTH, 4), dtype=np.uint8)
-dataset_next_img_sim = np.zeros((EPISODES, MAX_FRAMES, IMG_SIM_HEIGHT, IMG_SIM_WIDTH, 4), dtype=np.uint8)
+# dataset_current_pos_real = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)
+# dataset_current_vel_real = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)
+#
+# dataset_next_pos_sim = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)  # this is LSTM input
+# dataset_next_vel_sim = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)  # this is LSTM input
+#
+# dataset_next_pos_real = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)  # this is LSTM output
+# dataset_next_vel_real = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)  # this is LSTM output
+#
+# dataset_current_action = np.zeros((EPISODES, MAX_FRAMES, 6), dtype=np.float32)  # this is LSTM input/conditioning
+# dataset_current_speed = np.zeros((EPISODES, MAX_FRAMES), dtype=np.float32)
+#
+# dataset_current_img_real = np.zeros((EPISODES, MAX_FRAMES, IMG_REAL_HEIGHT, IMG_REAL_WIDTH, 4), dtype=np.uint8)
+#
+# dataset_next_img_real = np.zeros((EPISODES, MAX_FRAMES, IMG_REAL_HEIGHT, IMG_REAL_WIDTH, 4), dtype=np.uint8)
+# dataset_next_img_sim = np.zeros((EPISODES, MAX_FRAMES, IMG_SIM_HEIGHT, IMG_SIM_WIDTH, 4), dtype=np.uint8)
 
 ############## put future for loop here
 RECORDING = 1
@@ -154,20 +162,20 @@ for idx in range(len(real_imgs[RECORDING])):
     current_action = cmds[action_idx]
 
     if idx < len(real_imgs[RECORDING]) - 1:
-        current_real_img = real_img_reshaping(real_imgs[RECORDING][idx, :, :, :3])
-        next_real_img = real_img_reshaping(real_imgs[RECORDING][idx + 1, :, :, :3])
-        next_sim_img = np.rot90(venv.get_image(cam.handle), 2, (0, 1))
+        current_real_img = real_img_reshaping(real_imgs[RECORDING][idx, :, :, :])
+        next_real_img = real_img_reshaping(real_imgs[RECORDING][idx + 1, :, :, :])
+        next_sim_img = venv.get_image_and_depth(cam.handle)
 
         episode_current_pos_real[idx] = real_positions[RECORDING][idx, :, 0]  # index 0 are the positions
         episode_current_vel_real[idx] = real_positions[RECORDING][idx, :, 1]  # index 1 are the velocities
         # index 2 are the loads (unused)
 
-        next_pos, next_vel = get_motors(with_velocities=True)
+        next_pos, next_vel = get_motors()
         episode_next_pos_sim[idx] = next_pos
         episode_next_vel_sim[idx] = next_vel
 
-        episode_next_pos_real[idx] = real_positions[RECORDING][idx+1, :, 0]
-        episode_next_vel_real[idx] = real_positions[RECORDING][idx+1, :, 1]
+        episode_next_pos_real[idx] = real_positions[RECORDING][idx + 1, :, 0]
+        episode_next_vel_real[idx] = real_positions[RECORDING][idx + 1, :, 1]
 
         episode_current_action[idx] = current_action
         episode_current_speed[idx] = real_speeds[RECORDING][idx]
@@ -177,4 +185,16 @@ for idx in range(len(real_imgs[RECORDING])):
         episode_next_img_real[idx] = next_real_img
         episode_next_img_sim[idx] = next_sim_img
 
+f.create_dataset("episode_current_pos_real", data=episode_current_pos_real)
+f.create_dataset("episode_current_vel_real", data=episode_current_vel_real)
+f.create_dataset("episode_next_pos_sim", data=episode_next_pos_sim)
+f.create_dataset("episode_next_vel_sim", data=episode_next_vel_sim)
+f.create_dataset("episode_next_pos_real", data=episode_next_pos_real)
+f.create_dataset("episode_next_vel_real", data=episode_next_vel_real)
+f.create_dataset("episode_current_action", data=episode_current_action)
+f.create_dataset("episode_current_speed", data=episode_current_speed)
+f.create_dataset("episode_current_img_real", data=episode_current_img_real)
+f.create_dataset("episode_next_img_real", data=episode_next_img_real)
+f.create_dataset("episode_next_img_sim", data=episode_next_img_sim)
 
+f.close()
